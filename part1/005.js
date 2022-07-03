141c840ed2f72b9b2daa848f58e07c685cbef840
Antraxmin <antraxmin@naver.com>
2022-07-03 23:54:54 +0900

    update 22-07-03

---
+++ part1/005.js
@@ -0,0 +1,13 @@
+let name = "Peter"
+let number = 200
+let isTrue = true
+let nothing = null
+let empty = undefined
+let list = []
+let ref = {}
+let func = function() {}
+
+/* 
+변수 선언 - let 키워드 (var은 되도록 안쓰는게 좋음)
+동적 바인딩 - 런타임 시 변수의 값에 의해 동적으로 유형이 결정되는 것
+*/
